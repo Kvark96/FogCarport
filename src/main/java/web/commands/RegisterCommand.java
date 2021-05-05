bf15521f798106530f8530789f2b057046351017
Oliver Juul Reder <oliver.j.reder@gmail.com>
2021-05-05 10:58:09 +0200

    Mail must contain @ when registering

--- src/main/java/web/commands/RegisterCommand.java
+++ src/main/java/web/commands/RegisterCommand.java
@@ -26,6 +26,7 @@ public class RegisterCommand extends CommandUnprotectedPage
         String password1 = request.getParameter("password1");
         String password2 = request.getParameter("password2");
 
+        if(email.contains("@")){
         if (password1.equals(password2))
         {
             User user = userFacade.createUser(email, password1);
@@ -39,6 +40,10 @@ public class RegisterCommand extends CommandUnprotectedPage
         else
         {
             request.setAttribute("error", "the two passwords did not match");
+            return "registerpage";
+        }
+        } else {
+            request.setAttribute("error", "the email must be valid!");
             return "registerpage";
         }
     }
