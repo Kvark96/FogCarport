47a318cc6766af74b466ec90b594b990bc17181a
Sebastian Hejlesen <Cph-sh528@cphbusiness.dk>
2021-05-21 11:05:06 +0200

    TESTPAGES added

---
+++ src/test/java/business/persistence/RequestMapperTest.java
@@ -0,0 +1,12 @@
+package business.persistence;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RequestMapperTest {
+
+    @Test
+    void insertRequest() {
+    }
+}
