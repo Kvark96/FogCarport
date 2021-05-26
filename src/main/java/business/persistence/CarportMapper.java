d15b8aadc376665613364db3f3e4a1bbd7d3eaec
Oliver Juul Reder <oliver.j.reder@gmail.com>
2021-05-26 16:21:51 +0200

    Added poml

--- src/main/java/business/persistence/CarportMapper.java
+++ src/main/java/business/persistence/CarportMapper.java
@@ -76,6 +76,7 @@ public class CarportMapper  {
                 }
                 return standardCarportEntityList;
             } catch (SQLException e) {
+                System.out.println(e.getMessage());
                 throw new SQLException();
             }
         } catch (SQLException ex) {
@@ -83,46 +84,5 @@ public class CarportMapper  {
 
         }
         return standardCarportEntityList;
-    }
-
-
-    ///
-
-    private void addIdAndGetOrder_id(HttpServletRequest request, HttpServletResponse response) throws SQLException {
-        int user_id = (int) request.getSession().getAttribute("user_id");
-        int isARequest = (int) request.getSession().getAttribute("isARequest");
-        int length = (int) request.getSession().getAttribute("length");
-        int width = (int) request.getSession().getAttribute("width");
-        try (Connection connection = FrontController.database.connect()) {
-
-            String insertSql = "INSERT INTO carport.orders (user_id, customer_request,length,width) VALUES (?,?,?,?)";
-            try (PreparedStatement ps = connection.prepareStatement(insertSql)) {
-                ps.setInt(1, user_id);
-                ps.setInt(2, isARequest);
-                ps.setDouble(3, length);
-                ps.setDouble(4, width);
-                ps.execute();
-
-            } catch (SQLException error) {
-                System.out.println("Failed to add id to orders" + error.getMessage());
-            }
-
-
-            String orderSql = "SELECT order_id FROM carport.orders WHERE user_id = ? order by order_id desc";
-
-            try (PreparedStatement ps = connection.prepareStatement(orderSql)) {
-                ps.setInt(1, user_id);
-                ResultSet rs = ps.executeQuery();
-                rs.next();
-                request.setAttribute("order_id", rs.getInt("order_id")); // THIS WAS CHANGED FROM SESSIONSCOPE
-
-                bomMapper.generateCarport(rs.getInt("order_id"), length, width);
-
-                System.out.println(request.getSession().getAttribute("order_id"));
-            } catch (SQLException error) {
-                System.out.println("Failed get order_id from database=" + error.getMessage());
-            }
-        }
-
     }
 }
